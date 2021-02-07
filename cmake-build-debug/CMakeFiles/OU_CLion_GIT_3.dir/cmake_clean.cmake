1100002716301331707e6dbb38cedfa791ccab25
PGates2137 <235836@edu.p.lodz.pl>
2021-02-07 23:58:04 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OU_CLion_GIT_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_CLion_GIT_3.dir/main.cpp.obj"
+  "OU_CLion_GIT_3.exe"
+  "OU_CLion_GIT_3.exe.manifest"
+  "OU_CLion_GIT_3.pdb"
+  "libOU_CLion_GIT_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OU_CLion_GIT_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
